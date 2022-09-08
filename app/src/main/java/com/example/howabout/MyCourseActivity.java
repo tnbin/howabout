b090ba057a5c4ccc7df41bd08be1620e18026760
moble <lsjdltnqls1110@naver.com>
2022-09-08 16:53:05 +0900

    First Commit

---
+++ app/src/main/java/com/example/howabout/MyCourseActivity.java
@@ -0,0 +1,14 @@
+package com.example.howabout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyCourseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_course);
+    }
+}
---
+++ app/src/main/java/com/example/howabout/StoreInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.howabout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StoreInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.store_info);
+    }
+}
