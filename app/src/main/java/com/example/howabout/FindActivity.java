c616af1c07b54ea9df5f7e3993823df25676c68f
moble <lsjdltnqls1110@naver.com>
2022-09-26 14:45:02 +0900

    9 Commit

--- app/src/main/java/com/example/howabout/FindActivity.java
+++ app/src/main/java/com/example/howabout/FindActivity.java
@@ -2,9 +2,17 @@ package com.example.howabout;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
@@ -20,6 +28,7 @@ public class FindActivity extends AppCompatActivity {
     TextView textView;
     DrawerLayout drawerLayout;
     View drawerView;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +39,8 @@ public class FindActivity extends AppCompatActivity {
         textView = findViewById(R.id.textView);
         drawerLayout = findViewById(R.id.drawer_layout);
         drawerView = findViewById(R.id.drawer);
+        tv = findViewById(R.id.tv_location);
+        final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         ImageButton btn_open = findViewById(R.id.btn_open);
         btn_open.setOnClickListener(new View.OnClickListener() {
@@ -100,7 +111,67 @@ public class FindActivity extends AppCompatActivity {
                 startActivity(intentmc);
             }
         });
+
+
+        ImageButton mylocation = findViewById(R.id.mylocation);
+        mylocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Build.VERSION.SDK_INT >= 23 &&
+                        ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(FindActivity.this, new String[]{
+                            android.Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+                } else {
+                    // 가장최근 위치정보 가져오기
+                    Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    if (location != null) {
+                        String provider = location.getProvider();
+                        double longitude = location.getLongitude();
+                        double latitude = location.getLatitude();
+                        double altitude = location.getAltitude();
+
+                        tv.setText("위치정보 : " + provider + "\n" +
+                                "위도 : " + longitude + "\n" +
+                                "경도 : " + latitude + "\n" +
+                                "고도  : " + altitude);
+                    }
+
+                    // 위치정보를 원하는 시간, 거리마다 갱신해준다.
+                    lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                            1000,
+                            1,
+                            gpsLocationListener);
+                    lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                            1000,
+                            1,
+                            gpsLocationListener);
+                }
+            }
+        });
     }
+
+    final LocationListener gpsLocationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            // 위치 리스너는 위치정보를 전달할 때 호출되므로 onLocationChanged()메소드 안에 위지청보를 처리를 작업을 구현 해야합니다.
+            String provider = location.getProvider();  // 위치정보
+            double longitude = location.getLongitude(); // 위도
+            double latitude = location.getLatitude(); // 경도
+            double altitude = location.getAltitude(); // 고도
+            tv.setText("위치정보 : " + provider + "\n" + "위도 : " + longitude + "\n" + "경도 : " + latitude + "\n" + "고도 : " + altitude);
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
 
     DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
         @Override
