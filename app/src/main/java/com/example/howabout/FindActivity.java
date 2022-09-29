7c8279ef9f922551c11d951ce1cfb7b736fbe32c
moble <lsjdltnqls1110@naver.com>
2022-09-29 17:27:15 +0900

    SeekBar

--- app/src/main/java/com/example/howabout/FindActivity.java
+++ app/src/main/java/com/example/howabout/FindActivity.java
@@ -119,6 +119,9 @@ public class FindActivity extends AppCompatActivity implements MapView.CurrentLo
                 startActivity(intentmc);
             }
         });
+
+
+
         mapView = findViewById(R.id.map_view);
         mapView.setCurrentLocationEventListener(this);
 
@@ -148,22 +151,38 @@ public class FindActivity extends AppCompatActivity implements MapView.CurrentLo
             }
         });
 
-        ImageButton btn_up=findViewById(R.id.btn_up);
-        ImageButton btn_down=findViewById(R.id.btn_down);
-        btn_up.setOnClickListener(new View.OnClickListener() {
+        SeekBar seekBar=findViewById(R.id.seekbar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onClick(View view) {
-//                MapPoint getMapCenterPoint()
-//                mapView.setMapCenterPoint();
-//                mapView.setMapCenterPointAndZoomLevel();
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
             }
-        });
-        btn_down.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View view) {
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
 
             }
         });
+
+//        ImageButton btn_up = findViewById(R.id.btn_up);
+//        ImageButton btn_down = findViewById(R.id.btn_down);
+//        btn_up.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+//        btn_down.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
     }
 
     DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
@@ -188,52 +207,53 @@ public class FindActivity extends AppCompatActivity implements MapView.CurrentLo
         }
     };
 
-//현재위치 업데이트
+    //현재위치 업데이트
     @Override
     public void onCurrentLocationUpdate(MapView mapView, MapPoint mapPoint, float v) {
         MapPoint.GeoCoordinate mapPointGeo = mapPoint.getMapPointGeoCoord();
         Log.i("subin", String.format("MapView onCurrentLocationUpdate (%f,%f) accuracy (%f)", mapPointGeo.latitude, mapPointGeo.longitude, v));
         mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithHeading);
         MapPoint currentMapPoint = MapPoint.mapPointWithGeoCoord(mapPointGeo.latitude, mapPointGeo.longitude);
-        Log.i("subin",""+currentMapPoint);
+        Log.i("subin", "" + currentMapPoint);
         mapView.setMapCenterPoint(currentMapPoint, true);
         mCurrentLat = mapPointGeo.latitude;
         mCurrentLng = mapPointGeo.longitude;
-//        double x = mCurrentLat;
-//        double y = mCurrentLng;
-//        MapPoint mapPoint = MapPoint.mapPointWithGeoCoord(x, y);
-        MapCircle circle1 = new MapCircle(
-                MapPoint.mapPointWithGeoCoord(mCurrentLat,mCurrentLng), // center
-                500, // radius
-                Color.argb(128, 255, 87, 87), // strokeColor
-                Color.argb(0, 0, 0, 0) // fillColor
-        );
-        mapView.addCircle(circle1);
+
+        mapView.setCurrentLocationRadius(500);
+        mapView.setCurrentLocationRadiusStrokeColor(Color.argb(128, 255, 87, 87));
+        mapView.setCurrentLocationRadiusFillColor(Color.argb(0, 0, 0, 0));
+//        MapCircle circle1 = new MapCircle(
+//                MapPoint.mapPointWithGeoCoord(mCurrentLat, mCurrentLng), // center
+//                500, // radius
+//                Color.argb(128, 255, 87, 87), // strokeColor
+//                Color.argb(0, 0, 0, 0) // fillColor
+//        );
+//        mapView.addCircle(circle1);
         if (!isTrackingMode) {
             mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOff);
         }
-        JSONObject location=new JSONObject();
+        JSONObject location = new JSONObject();
         try {
-            location.put("lat",mCurrentLat);
-            location.put("lng",mCurrentLng);
-            location.put("radius",500);
+            location.put("lat", mCurrentLat);
+            location.put("lng", mCurrentLng);
+            location.put("radius", 500);
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        ArrayList<JSONObject> arrayList=new ArrayList<>();
+        ArrayList<JSONObject> arrayList = new ArrayList<>();
         arrayList.add(location);
-        Call<Integer> testlocation=RetrofitClient.getApiService().testlocation(arrayList);
+        Call<Integer> testlocation = RetrofitClient.getApiService().testlocation(arrayList);
         testlocation.enqueue(new Callback<Integer>() {
             @Override
             public void onResponse(Call<Integer> call, Response<Integer> response) {
-                Integer test=response.body();
-                Log.i("subin",""+test);
+                Integer test = response.body();
+                Log.i("subin", "" + test);
             }
 
             @Override
             public void onFailure(Call<Integer> call, Throwable t) {
 
-                Log.i("subin",""+t.getMessage());
+                Log.i("subin", "" + t.getMessage());
             }
         });
     }
