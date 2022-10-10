2692b74ed834af09fddc8696bb6b37c9dd5317a4
moble <lsjdltnqls1110@naver.com>
2022-10-10 21:15:46 +0900

    10월10일

--- app/src/main/java/com/example/howabout/FindActivity.java
+++ app/src/main/java/com/example/howabout/FindActivity.java
@@ -649,6 +649,7 @@ public class FindActivity extends AppCompatActivity implements MapView.CurrentLo
                         @Override
                         public void onResponse(Call<ArrayList<JSONObject>> call, Response<ArrayList<JSONObject>> response) {
                             Log.i("subin","rest 연결성공"+response.body());
+
                         }
 
                         @Override
