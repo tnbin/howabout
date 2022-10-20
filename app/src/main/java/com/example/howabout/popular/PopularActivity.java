4d4cc4b90177a17ce56951cd72b582f126956d30
moble <lsjdltnqls1110@naver.com>
2022-10-20 19:11:49 +0900

    popular 초기화

--- app/src/main/java/com/example/howabout/popular/PopularActivity.java
+++ app/src/main/java/com/example/howabout/popular/PopularActivity.java
@@ -46,7 +46,7 @@ public class PopularActivity extends AppCompatActivity {
     ArrayList<JSONObject> popularlist = new ArrayList<JSONObject>();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.popular);
 
