5472f05eea1c54c7c09b8791d499fdcaef532cfe
moble <lsjdltnqls1110@naver.com>
2022-10-20 18:02:46 +0900

    popular 초기화

--- app/src/main/java/com/example/howabout/popular/PopularActivity.java
+++ app/src/main/java/com/example/howabout/popular/PopularActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.howabout.API.RetrofitClient;
 import com.example.howabout.FindActivity;
@@ -35,18 +37,23 @@ public class PopularActivity extends AppCompatActivity {
     SharedPreferences preferences;
     View drawerView;
     ListView listView;
-    PopularAdapter popularAdapter=new PopularAdapter();;
-    ArrayList<Popular_item> popular_items=new ArrayList<Popular_item>();;
-    List<String> urllist=new ArrayList<>();
-    List<String> textlist=new ArrayList<>();
+    PopularAdapter popularAdapter = new PopularAdapter();
+    ArrayList<Popular_item> popular_items = new ArrayList<Popular_item>();
+    List<String> urllist;
+    List<String> textlist;
     Popular_item popular_item;
     //인기코스 리스트
-    ArrayList<JSONObject>popularlist=new ArrayList<JSONObject>();
+    ArrayList<JSONObject> popularlist = new ArrayList<JSONObject>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.popular);
 
+        //인기차트 들어갈때 oncreate시 전체 인기코스 불러오기
+//        popularAdapter.clear();
+//        //서버 연결
+//        popularcourse("전체", "전체", "전체", "전체");
         /////////////////////drawerlayout start
         drawerLayout = findViewById(R.id.drawer_layout);
         drawerView = findViewById(R.id.drawer);
@@ -112,42 +119,64 @@ public class PopularActivity extends AppCompatActivity {
 
             }
         });
-        Button btn_age=findViewById(R.id.btn_age);
-        final BottomSheet_age bottomSheet_age=new BottomSheet_age(getApplicationContext());
+        Button btn_age = findViewById(R.id.btn_age);
+        final BottomSheet_age bottomSheet_age = new BottomSheet_age(getApplicationContext());
         btn_age.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                bottomSheet_age.show(getSupportFragmentManager(),bottomSheet_age.getTag());
+                bottomSheet_age.show(getSupportFragmentManager(), bottomSheet_age.getTag());
             }
         });
-        Button btn_region=findViewById(R.id.btn_region);
-        final BottomSheet_region bottomSheet_region =new BottomSheet_region(getApplicationContext());
+        Button btn_region = findViewById(R.id.btn_region);
+        final BottomSheet_region bottomSheet_region = new BottomSheet_region(getApplicationContext());
         btn_region.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                bottomSheet_region.show(getSupportFragmentManager(),bottomSheet_region.getTag());
+                bottomSheet_region.show(getSupportFragmentManager(), bottomSheet_region.getTag());
             }
         });
         /////////////////////drawerlayout finish
         //검색 버튼 눌렀을 때 
-        Button btn_casearch=findViewById(R.id.btn_casearch);
+        Button btn_casearch = findViewById(R.id.btn_casearch);
         btn_casearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Bottomsheet에서 저장된 값 (성별,나이,도,시) 꺼내기
-                preferences =getSharedPreferences("popular", MODE_PRIVATE);
-                String gen=preferences.getString("p_gender","x");
-                String age=preferences.getString("p_age","x");
-                String region_do=preferences.getString("do","x");
-                String region_si=preferences.getString("si","x");
+                preferences = getSharedPreferences("popular", MODE_PRIVATE);
+                String gen = preferences.getString("p_gender", "전체");
+                String age = preferences.getString("p_age", "전체");
+                String region_do = preferences.getString("do", "전체");
+                String region_si = preferences.getString("si", "전체");
+                Log.i("subin",gen+","+age+","+region_do+","+region_si);
                 //검색할때마다 값을 불러오지 않게 값 초기화
+                urllist= new ArrayList<>();
+                textlist= new ArrayList<>();
                 popularAdapter.clear();
+                popularAdapter.notifyDataSetChanged();
                 //서버 연결
-                popularcourse(gen,age,region_do,region_si);
+                popularcourse(gen, age, region_do, region_si);
             }
         });
-        popularAdapter=new PopularAdapter();
+
     }
+
+    //앱의 activity을 벗어났을때 저장된 값을 삭제하기 어려워 preference에 저장시킴.
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        SharedPreferences preferences=getSharedPreferences("popular", MODE_PRIVATE);
+        SharedPreferences.Editor edit = preferences.edit();
+        edit.putString("do", "전체");
+        edit.putString("si","전체");
+        edit.putString("p_age", "전체");
+        edit.putString("p_gender", "전체");
+        edit.apply();
+        edit.commit();
+
+    }
+
+
     DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
         @Override
         public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
@@ -169,59 +198,71 @@ public class PopularActivity extends AppCompatActivity {
 
         }
     };
-    public void popularcourse(String gender,String age,String location_do,String location_si){
-        JSONObject popular=new JSONObject();
+
+    public void popularcourse(String gender, String age, String location_do, String location_si) {
+        JSONObject popular = new JSONObject();
         try {
-            popular.put("gender",gender);
-            popular.put("age",age);
-            popular.put("location_do",location_do);
-            popular.put("location_si",location_si);
+            popular.put("gender", gender);
+            popular.put("age", age);
+            popular.put("location_do", location_do);
+            popular.put("location_si", location_si);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        ArrayList<JSONObject> jsonObjectArrayList=new ArrayList<>();
+        ArrayList<JSONObject> jsonObjectArrayList = new ArrayList<>();
         jsonObjectArrayList.add(popular);
-        Call<ArrayList<JSONObject>> polcourse= RetrofitClient.getApiService().popular(jsonObjectArrayList);
+        Call<ArrayList<JSONObject>> polcourse = RetrofitClient.getApiService().popular(jsonObjectArrayList);
         polcourse.enqueue(new Callback<ArrayList<JSONObject>>() {
             @Override
             public void onResponse(Call<ArrayList<JSONObject>> call, Response<ArrayList<JSONObject>> response) {
                 //서버에서 받은 값 popularlist에 저장
-                popularlist=response.body();
+                popularlist = response.body();
+                String str = popularlist.toString();
+                Log.i("subin", "서버에서 준 값" + str);
+
                 //서버에서 받은 값 키값으로 불러오기 위해서 jsonobject 생성
                 JSONObject jsonObject;
                 try {
-                    //불러온 값 크기에 맞추어 list에 저장 size
-                    for (int i=0;i<popularlist.size();i++){
-                        //jsonobject에 list 값 끝까지 저장
-                        jsonObject=popularlist.get(i);
-                        //보여줄 listview 불러오기
-                        listView=findViewById(R.id.popular_list);
-                        //arraylist url에 서버에서 받은 값 (카페 url) 저장
-                        urllist.add(jsonObject.get("c_image_url").toString());
-                        //arraylist text에 서버에서 받은 값 (카페 text) 저장
-                        textlist.add(jsonObject.get("c_do").toString()+" "+jsonObject.get("c_si")+" 코스");
-                        //객체 불러오기 (list custom)
-                        popular_item=new Popular_item();
-                        //객체에 맞게 list에 저장된값 저장
-                        popular_item.setPlace(textlist.get(i));
-                        popular_item.setImage(urllist.get(i));
-                        //adapter에 값이 저장된 객체를 추가
-                        popularAdapter.addItem(popular_item);
+                    if (popularlist.size() == 0) {
+                        Toast.makeText(PopularActivity.this, "인기 코스가 없습니다", Toast.LENGTH_SHORT).show();
+                    } else {
+                        //불러온 값 크기에 맞추어 list에 저장 size
+                        for (int i = 0; i < popularlist.size(); i++) {
+                            //jsonobject에 list 값 끝까지 저장
+                            jsonObject = popularlist.get(i);
+                            //보여줄 listview 불러오기
+                            listView = findViewById(R.id.popular_list);
+                            //arraylist url에 서버에서 받은 값 (카페 url) 저장
+                            urllist.add(jsonObject.get("c_image_url").toString());
+                            Log.i("subin", "서버에서 받은 rest text 값: " + jsonObject.get("c_do").toString() + ", " + i);
+                            Log.i("subin", "서버에서 받은 rest text 값: " + jsonObject.get("c_si").toString() + ", " + i);
+                            //arraylist text에 서버에서 받은 값 (카페 text) 저장
+                            textlist.add(jsonObject.get("c_do").toString() + " " + jsonObject.get("c_si") + " 코스");
+                            Log.i("subin","textlist 값: "+textlist);
+                            //객체 불러오기 (list custom)
+                            popular_item = new Popular_item();
+                            //객체에 맞게 list에 저장된값 저장
+                            popular_item.setPlace(textlist.get(i));
+                            popular_item.setImage(urllist.get(i));
+                            //adapter에 값이 저장된 객체를 추가
+                            popularAdapter.addItem(popular_item);
 //                        Log.i("subin","imageurl: "+urllist);
 //                        Log.i("subin","imageurl: "+textlist);
-                        Log.i("subin",popular_item+"hhhhh"+i);
-                        //listview와 adapter 연결
-                        listView.setAdapter(popularAdapter);
-                        //adapter 업데이트
-                        popularAdapter.notifyDataSetChanged();
+                            Log.i("subin", popular_item + "hhhhh" + i);
+                            //listview와 adapter 연결
+                            listView.setAdapter(popularAdapter);
+                            //adapter 업데이트
+                            popularAdapter.notifyDataSetChanged();
+                        }
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
+
             @Override
             public void onFailure(Call<ArrayList<JSONObject>> call, Throwable t) {
-                Log.i("subin","popular sever 연결 실패"+t.getMessage());
+                Log.i("subin", "popular sever 연결 실패" + t.getMessage());
             }
         });
     }
